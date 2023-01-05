f1c489a95b7d74bb0384e8c990b679eb31355d6f
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2023-01-05 12:23:00 +0530

    1 file modified, 2 added

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { useReducer, useEffect } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
+import { JSONPLACEHOLDER_API_URL } from '../../helpers/config';
+import { getJson } from '../../helpers/helperFns';
+import { ACTION_TYPES } from '../../helpers/actions';
+import { iState, reducer } from '../../reducers/jsonPlaceholderReducer';
 import './HomePage.scss';
 
 const HomePage = () => {
+  const [state, dispatch] = useReducer(reducer, iState);
+  console.log(state);
+
+  const { isLoading, jsonPlaceholderData, isError, jsonPlaceholderPram } =
+    state;
+
+  useEffect(() => {
+    dispatch({
+      type: ACTION_TYPES.FETCH_START,
+    });
+
+    (async () => {
+      try {
+        const data = await getJson(
+          `${JSONPLACEHOLDER_API_URL}/${jsonPlaceholderPram}`
+        );
+        dispatch({
+          type: ACTION_TYPES.FETCH_SUCCESS,
+          payload: [...data],
+        });
+      } catch (err) {
+        dispatch({
+          type: ACTION_TYPES.FETCH_ERROR,
+          payload: err,
+        });
+      }
+    })();
+
+    console.log('useEffect runs...');
+
+    return () => {
+      console.log('Clean Up...');
+    };
+  }, []);
+
   return (
     <section className="home-page">
       <Container>
---
+++ src/reducers/jsonPlaceholderReducer.js
@@ -0,0 +1,32 @@
+import { ACTION_TYPES } from '../helpers/actions';
+
+export const iState = {
+  isLoading: false,
+  jsonPlaceholderData: [],
+  isError: null,
+  jsonPlaceholderPram: 'posts',
+};
+
+export const reducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.FETCH_START:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case ACTION_TYPES.FETCH_SUCCESS:
+      return {
+        ...state,
+        jsonPlaceholderData: action.payload,
+        isLoading: false,
+      };
+    case ACTION_TYPES.FETCH_ERROR:
+      return {
+        ...state,
+        isError: action.payload,
+        isLoading: false,
+      };
+    default:
+      return { ...state };
+  }
+};
