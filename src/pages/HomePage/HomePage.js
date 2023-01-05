13e75636a2e15a24aa660ed3630ff897c88e68ca
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2023-01-05 11:40:10 +0530

    1 file modified

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,2 +1,17 @@
 import React from 'react';
+import { Container, Col, Row } from 'react-bootstrap';
 import './HomePage.scss';
+
+const HomePage = () => {
+  return (
+    <section className="home-page">
+      <Container>
+        <Row>
+          <Col md={12}>Homepage...</Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default HomePage;
