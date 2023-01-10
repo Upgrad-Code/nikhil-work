75e9ebd11c2f2798db0ea2c72ed47b1f0fe47f24
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2023-01-10 22:46:12 +0530

    3 files modified

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Container, Col, Row, Card, Button } from 'react-bootstrap';
+import './Product.scss';
 
 export const Product = ({ data }) => {
   console.log(data);
@@ -10,7 +11,7 @@ export const Product = ({ data }) => {
         data.length > 0 &&
         data.map((p) => {
           return (
-            <Col lg={3} md={4} key={p.id} className="mb-4">
+            <Col lg={3} md={4} key={p.id} className="mb-4 product">
               <Card>
                 <Card.Img variant="top" src={p.thumbnail} />
                 <Card.Body>
