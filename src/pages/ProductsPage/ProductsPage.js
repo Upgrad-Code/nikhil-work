e0d75b2c81a5c6c0d2ff3643924d940574f7f6aa
Upgrad-Code <112937791+Upgrad-Code@users.noreply.github.com>
2023-01-10 15:40:02 +0530

    2 files modified

--- src/pages/ProductsPage/ProductsPage.js
+++ src/pages/ProductsPage/ProductsPage.js
@@ -1,19 +1,47 @@
 import React, { useEffect, useReducer, useContext } from 'react';
 import { Container, Col, Row } from 'react-bootstrap';
 import { DUMMY_PRODUCTS_API_URL } from '../../helpers/config';
+import { ACTION_TYPES } from '../../helpers/actions';
 import { getJson } from '../../helpers/helperFns';
 import { iState, reducer } from '../../reducers/productsPageReducer';
 import './ProductsPage.scss';
 
 const ProductsPage = () => {
   const [state, dispatch] = useReducer(reducer, iState);
-  console.log(state, dispatch);
+  console.log(state);
 
   useEffect(() => {
+    let subscribed = true;
+
+    dispatch({
+      type: ACTION_TYPES.FETCH_START,
+    });
+
     (async () => {
-      const data = await getJson(DUMMY_PRODUCTS_API_URL);
+      try {
+        const data = await getJson(DUMMY_PRODUCTS_API_URL);
+        console.log(data);
+        if (subscribed) {
+          dispatch({
+            type: ACTION_TYPES.FETCH_SUCCESS,
+            payload: [...data.products],
+          });
+        }
+      } catch (err) {
+        if (subscribed) {
+          dispatch({
+            type: ACTION_TYPES.FETCH_ERROR,
+            payload: err,
+          });
+        }
+      }
     })();
-  });
+
+    return () => {
+      subscribed = false;
+    };
+  }, []);
+
   return (
     <Container>
       <Row>
--- src/reducers/productsPageReducer.js
+++ src/reducers/productsPageReducer.js
@@ -1,19 +1,19 @@
 import { ACTION_TYPES } from '../helpers/actions';
 
 export const iState = {
-  isLoading: false,
+  loading: false,
   products: [],
-  isError: null,
+  error: null,
 };
 
 export const reducer = (state, action) => {
   switch (action.type) {
     case ACTION_TYPES.FETCH_START:
-      return state;
+      return { ...state, loading: true };
     case ACTION_TYPES.FETCH_SUCCESS:
-      return state;
+      return { ...state, products: action.payload, loading: false };
     case ACTION_TYPES.FETCH_ERROR:
-      return state;
+      return { ...state, error: action.payload, loading: false };
     default:
       return state;
   }
